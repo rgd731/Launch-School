8b161212fb06e9b5354dc86609d3a393018d10b1
Ricardo Delgado <95548929+rgd731@users.noreply.github.com>
2023-08-26 10:51:05 -0500

    Added Bonus Features to JS101 Lesson 2 Rock Paper Scissors Program

--- JS101/Lesson-2/Rock-Paper-Scissors.js
+++ JS101/Lesson-2/Rock-Paper-Scissors.js
@@ -1,27 +1,50 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock','paper','scissors'];
+const VALID_CHOICES = ['rock','paper','scissors','lizard','spock'];
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
+let userWins = 0;
+let computerWins = 0;
 
 function prompt(msg,additionalString = '') {
   process.stdout.write(`${msg}\n> `);
   return readline.question(additionalString);
 }
+function playerWins (userChoice,computerChoice) {
+  return WINNING_COMBOS[userChoice].includes(computerChoice);
+}
 function displayWinner(userChoice,computerChoice) {
   console.log(`You chose ${userChoice}, computer chose ${computerChoice}.`);
-  if ((userChoice === 'rock' && computerChoice === 'scissors') ||
-      (userChoice === 'paper' && computerChoice === 'rock') ||
-      (userChoice === 'scissors' && computerChoice === 'paper')) {
+  if (playerWins(userChoice,computerChoice)) {
     console.log('You win!');
-  } else if ((userChoice === 'rock' && computerChoice === 'paper') ||
-          (userChoice === 'paper' && computerChoice === 'scissors') ||
-          (userChoice === 'scissors' && computerChoice === 'rock')) {
-    console.log('Computer wins!');
-  } else {
+  } else if (userChoice === computerChoice) {
     console.log("It's a tie!");
+  } else {
+    console.log("Computer wins!");
+  }
+}
+function updateScore(userChoice,computerChoice) {
+  if (playerWins(userChoice,computerChoice)) {
+    userWins++;
+  } else if (userChoice === computerChoice) {
+    userWins++;
+    computerWins++;
+  } else {
+    computerWins++;
   }
 }
 
 while (true) {
-  let userChoice = prompt("Choose one: Rock, Paper, Scissors").toLowerCase();
+  if (!userWins && !computerWins) {
+    console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
+    console.log("First to 5 wins!\n");
+  }
+
+  let userChoice = prompt("Choose one: Rock, Paper, Scissors, Lizard, Spock").toLowerCase();
   while (!VALID_CHOICES.includes(userChoice)) {
     userChoice = prompt("That was not a valid choice. Please choose again").toLowerCase();
   }
@@ -30,11 +53,19 @@ while (true) {
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(userChoice,computerChoice);
+  updateScore(userChoice,computerChoice);
+  console.log(`Current score is User: ${userWins}  Computer: ${computerWins}\n`);
 
-  let answer = prompt("Would you like to play again?");
-  console.log(answer);
-  while (answer[0] !== 'y' && answer[0] !== 'n') {
-    answer = prompt("Please enter y or n.");
+  if (userWins >= 3 || computerWins >= 3) {
+    console.log(`${userWins >= 3 ? 'You are' : 'Computer is'} the grand winner!`);
+
+    let answer = prompt("Would you like to play again?").toLowerCase();
+    while (answer[0] !== 'y' && answer[0] !== 'n') {
+      answer = prompt("Please enter y or n.");
+    }
+    if (answer[0] === 'n') break;
+
+    userWins = 0;
+    computerWins = 0;
   }
-  if (answer[0] === 'n') break;
 }
